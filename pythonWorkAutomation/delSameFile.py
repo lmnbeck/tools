217438ab172a8e98c309823e20f4dc7a92800904
lmnbeck <maniagea09@gmail.com>
2021-04-30 17:11:34 +0800

    add delSameFile.py and processbar

--- pythonWorkAutomation/delSameFile.py
+++ pythonWorkAutomation/delSameFile.py
@@ -1,5 +1,7 @@
+import os
 from pathlib import Path
 from filecmp import cmp
+from processBar import *
 
 #  Define the folders to compare
 src_folder = Path(input("where?").strip())
@@ -13,15 +15,30 @@ srcFiles = list(src_folder.rglob('*'))
 # Judge if it is a file,to reduce the for loop rounds
 file_list = []
 for i in srcFiles:
-    if i.is_file():
-        file_list.append(i) 
+    try: 
+        if i.is_file():
+            file_list.append(i) 
+    except WindowsError:
+        print("winError....")
+    else:
+        continue
+
+process_bar = ShowProcess(len(file_list))
 
 for m in srcFiles:
+    process_bar.show_process()
     for n in srcFiles:
         #  find the same file name in list
-        if m != n and m.exists() and n.exists():
-            if cmp(m,n):
-                print(n, " has same file as ", m)
-                #  move the same file to des_folder
-                n.replace(des_folder / n.name)
-                print("move ",n ,"to ",des_folder)
+        try: 
+            if m != n and m.exists() and n.exists() and os.path.getsize(m):
+                if cmp(m,n):
+                    print(n, " has same file as ", m)
+                    #  move the same file to des_folder
+                    # n.replace(des_folder / n.name)
+                    print("move ",n ,"to ",des_folder)
+        except WindowsError:
+            print("winError....")
+        else:
+            continue
+
+process_bar.close('donzo')
