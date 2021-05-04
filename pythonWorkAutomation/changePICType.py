4102ee0dc3f4927e7138a02e4acae2f607b62663
ubuntu <ubuntu@localhost.localdomain>
2021-05-05 07:12:17 +0800

    chang picture type worked.

--- pythonWorkAutomation/changePICType.py
+++ pythonWorkAutomation/changePICType.py
@@ -1,16 +1,16 @@
 from pathlib import Path
 from PIL import Image
 
-scr_folder = Path('e:\\pic\\before\\')
-des_folder = Path('e:\\pic\\after\\')
+scr_folder = Path('/home/ubuntu/workSpace/pic/before/')
+des_folder = Path('/home/ubuntu/workSpace/pic/after/')
 if not des_folder.exists():
     des_folder.mkdir(parents = True)
 
 file_list = list(scr_folder.glob('*.jpg'))
 for i in file_list:
     des_file = des_folder / i.name
-    des_file = des_file.with_suffix(".png")
-    Image.open(i).save(des_folder)
+    des_file = des_file.with_suffix(".PNG")
+    Image.open(i).save(des_file)
     print("%s finished type chang " % i.name)
 
 
