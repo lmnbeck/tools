d4a74ca1749656a5b1fc338d4ff86ef541ba7d0c
ubuntu <ubuntu@localhost.localdomain>
2021-05-10 16:47:22 +0800

    linux cant run chromedriver

--- pythonWorkAutomation/docMemo.py
+++ pythonWorkAutomation/docMemo.py
@@ -55,3 +55,79 @@ print(price)
 str1 = 'xyzxyz-Python-zyxzyx'
 str2 = str1.rstrip('zyx')
 print(str2)
+
+# 4 自动化处理PDF文件
+## python版本切换：2.1. 将 python 各版本添加到 update-alternatives
+'''
+2.1. 将 python 各版本添加到 update-alternatives
+$ which python3.8
+/usr/bin/python3.8
+
+$ sudo update-alternatives --install /usr/bin/python3 python3 /usr/bin/python3.8 1
+
+$ which python3.5
+/usr/bin/python3.5
+
+$ sudo update-alternatives --install /usr/bin/python3 python3 /usr/bin/python3.5 2
+
+2.2. 配置 python3 默认指向 python3.8
+$ sudo update-alternatives --config python3
+
+
+There are 2 choices for the alternative python3 (providing /usr/bin/python3).
+
+  Selection    Path                Priority   Status
+------------------------------------------------------------
+* 0            /usr/bin/python3.5   2         auto mode
+  1            /usr/bin/python3.5   2         manual mode
+  2            /usr/bin/python3.8   1         manual mode
+
+Press <enter> to keep the current choice[*], or type selection number: 2
+
+选择/输入 2, 回车。
+
+二、卸载python3.8
+1、卸载python3.8
+sudo apt-get remove python3.8
+1
+2、卸载python3.8及其依赖
+sudo apt-get remove --auto-remove python3.8
+1
+3、清除python3.8
+sudo apt-get purge python3.8
+or
+sudo apt-get purge --auto-remove python3.8
+
+注释：
+
+此方法卸载python比较彻底，所以适合更换python版本时使用。
+'''
+## 解决chromedriver问题
+''' error message:
+Exception has occurred: WebDriverException
+Message: 'chromedriver' executable needs to be in PATH. Please see https://sites.google.com/a/chromium.org/chromedriver/home
+  File "/home/ubuntu/workSpace/tools/pythonWorkAutomation/downloadPDF.py", line 5, in <module>
+    browser = webdriver.Chrome()
+'''
+### 找到chrome版本
+'''
+安装：
+sudo apt-get install -y chromium-browser
+
+安装信息最后：
+Setting up chromium-browser-l10n (90.0.4430.72-0ubuntu0.18.04.1) ...
+**90.0.4430.72**是版本号
+
+去http://chromedriver.storage.googleapis.com/index.html 网站找到相应的版本驱动
+ubuntu 安装chromedriver
+
+// 下载对应的版本
+wget -N https://chromedriver.storage.googleapis.com/91.0.4472.19/chromedriver_linux64.zip
+// 解压
+unzip chromedriver_linux64.zip
+chmod +x chromedriver
+// 复制文件
+cp chromedriver /usr/bin/chromedriver
+————————————————
+
+'''
--- pythonWorkAutomation/downloadPDF.py
+++ pythonWorkAutomation/downloadPDF.py
@@ -7,3 +7,5 @@ url = 'http://www.cninfo.com.cn/new/disclosure/stock?stockCode=002594&orgId=gshk
 browser.get(url)
 time.sleep(3)
 
+#   WARNING: The scripts pip, pip3 and pip3.8 are installed in '/home/ubuntu/.local/bin' which is not on PATH.
+#   Consider adding this directory to PATH or, if you prefer to suppress this warning, use --no-warn-script-location.
