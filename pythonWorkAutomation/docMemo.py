8375f8a41fd32378280d0def2687b7637487be0b
ubuntu <ubuntu@localhost.localdomain>
2021-05-10 17:19:02 +0800

    rename file classification and update chromedriver

--- pythonWorkAutomation/docMemo.py
+++ pythonWorkAutomation/docMemo.py
@@ -130,4 +130,10 @@ chmod +x chromedriver
 cp chromedriver /usr/bin/chromedriver
 ————————————————
 
+## 添加代码使得chrome正常运行
+chrome_options = webdriver.ChromeOptions()
+chrome_options.add_argument('--headless')
+chrome_options.add_argument('--disable-gpu')
+chrome_options.add_argument("window-size=1024,768")
+chrome_options.add_argument("--no-sandbox")
 '''
